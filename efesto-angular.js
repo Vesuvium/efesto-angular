f9c3e3726df5bc0d93b24f9afe2ba9cc1ee4dd63
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-02-20 15:05:28 +0100

    style(api): reflow indentation

--- efesto-angular.js
+++ efesto-angular.js
@@ -5,116 +5,125 @@
 (function(){
     'use strict';
 
-    angular.module('efesto.angular').factory('api', ['$http', '$resource',
-      function($http, $resource){
-        var api = {};
+    angular
+        .module('efesto.angular')
+        .factory('api', ['$http', '$resource', api]);
 
-        api.domain = 'http://127.0.0.1:8000';
-        api.users = {};
+        function api($http, $resource) {
+            var api = {
+                domain: 'http://127.0.0.1:8000',
+                users: {},
+                addUser: addUser,
+                addToken: addToken,
+                getToken: getToken,
+                login: login,
+                headers: headers,
+                collection: collection,
+                resource: resource,
+                isAuthenticated: isAuthenticated
+            };
+            return api;
 
-        api.addUser = function(name, password){
-          api.users[name] = {'username': name, 'password': password};
-        };
-
-        api.addToken = function(user, token){
-          if (api.users.hasOwnProperty(user) === false){
-            api.users[user] = {};
-          }
-          api.users[user].token = token;
-        };
-
-        api.getToken = function(user){
-          if (api.users.hasOwnProperty(user)){
-            if (api.users[user].hasOwnProperty('token')){
-              return api.users[user].token;
+            function addUser(name, password) {
+                api.users[name] = {'username': name, 'password': password};
             }
-          }
-          return false;
-        };
 
-        api.login = function(user, success, failure){
-          if (api.users.hasOwnProperty(user) === false){
-            throw Error("Missing credentials");
-          }
-
-          $http({
-            method: 'POST',
-            url: api.domain + '/auth',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            data: 'username=' + api.users[user].username + '&password='+encodeURIComponent(api.users[user].password)
-          })
-          .then(
-            function(response){
-              api.addToken(user, response.data.token);
-              api.currentUser = user;
-              if (success){
-                success(response);
-              }
-            },
-            function(response){
-              if (failure){
-                failure(response);
-              }
+            function addToken(user, token) {
+                if (api.users.hasOwnProperty(user) === false) {
+                    api.users[user] = {};
+                }
+                api.users[user].token = token;
             }
-          );
-      };
 
-      api.headers = function(){
-        var headers = {};
-        if ( api.currentUser ){
-          var token = api.getToken(api.currentUser);
-          var auth = window.btoa('any:' + decodeURIComponent(token));
-          headers.Authorization =  'Basic ' + auth;
+            function getToken(user) {
+                if (api.users.hasOwnProperty(user)){
+                    if (api.users[user].hasOwnProperty('token')) {
+                        return api.users[user].token;
+                    }
+                }
+                return false;
+            }
+
+            function login(user, success, failure) {
+                if (api.users.hasOwnProperty(user) === false) {
+                    throw Error("Missing credentials");
+                }
+
+                $http({
+                    method: 'POST',
+                    url: api.domain + '/auth',
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    data: 'username=' + api.users[user].username + '&password='+encodeURIComponent(api.users[user].password)
+                })
+                .then(
+                    function(response) {
+                        api.addToken(user, response.data.token);
+                        api.currentUser = user;
+                        if (success) {
+                            success(response);
+                        }
+                    },
+                    function(response) {
+                        if (failure) {
+                            failure(response);
+                        }
+                    }
+                );
+            }
+
+            function headers() {
+                var headers = {};
+                if (api.currentUser) {
+                    var token = api.getToken(api.currentUser);
+                    var auth = window.btoa('any:' + decodeURIComponent(token));
+                    headers.Authorization =  'Basic ' + auth;
+                }
+                return headers;
+            }
+
+            function collection(endpoint) {
+                var headers = api.headers();
+                return $resource(api.domain + '/' + endpoint, {}, {
+                    get: {
+                        method: 'GET',
+                        headers: headers
+                    },
+                    post: {
+                        method: 'POST',
+                        headers: headers
+                    }
+                });
+            }
+
+            function resource(endpoint) {
+                var headers = api.headers();
+                return $resource(api.domain + '/' + endpoint + '/:id', {id: '@id'}, {
+                    patch: {
+                        method: 'PATCH',
+                        headers: headers
+                    },
+                    get: {
+                        method: 'GET',
+                        headers: headers
+                    },
+                    delete: {
+                        method: 'DELETE',
+                        headers: headers
+                    }
+                });
+            }
+
+            function isAuthenticated(user) {
+                if (api.currentUser === user) {
+                    return true;
+                }
+                if (api.users.hasOwnProperty(user)) {
+                    if (api.users[user].token){
+                    return true;
+                    }
+                }
+                return false;
+            }
+
         }
-        return headers;
-      };
-
-      api.collection = function(endpoint){
-        var headers = api.headers();
-        return $resource(api.domain + '/' + endpoint, {}, {
-          get: {
-            method: 'GET',
-            headers: headers
-          },
-          post: {
-            method: 'POST',
-            headers: headers
-          }
-        });
-      };
-
-
-      api.resource = function(endpoint){
-        var headers = api.headers();
-        return $resource(api.domain + '/' + endpoint + '/:id', {id: '@id'}, {
-          patch: {
-            method: 'PATCH',
-            headers: headers
-          },
-          get: {
-            method: 'GET',
-            headers: headers
-          },
-          delete: {
-            method: 'DELETE',
-            headers: headers
-          }
-        });
-      };
-
-      api.isAuthenticated = function(user){
-        if (api.currentUser === user){
-          return true;
-        }
-        if (api.users.hasOwnProperty(user)){
-          if (api.users[user].token){
-            return true;
-          }
-        }
-        return false;
-      };
-
-      return api;
-    }
-  ]);
 })();
